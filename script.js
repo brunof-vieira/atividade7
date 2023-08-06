aacdacd9ccd23bb701081d26b62bebe053c5e237
brunof-vieira <bruno_vieirasm@hotmail.com>
2023-08-06 20:49:49 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+// Funsão sem argumento de retorno 
+function exibirMensagem() {
+    console.log("Olá!");
+}
+exibirMensagem();
+
+
+// Função com argumento e retorno 
+function Media(nodaUm, notaDois) {
+    return(nodaUm + notaDois) / 2;
+
+}
+console.log("A media é:" + Media(9,5)); 
